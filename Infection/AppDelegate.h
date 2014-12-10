51ab65c84b159ab5ac533c1f5c4b5ee70d4f869c
Justin Ehlert <justin.ehlert@bottlerocketapps.com>
2014-12-09 18:57:03 -0600

    Initial Commit

---
+++ Infection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Infection
+//
+//  Created by Justin Ehlert on 12/9/14.
+//  Copyright (c) 2014 Justin Ehlert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
