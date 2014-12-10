f7545c9b3782b324f39f35795449311e3466a30f
Justin Ehlert <justin.ehlert@utdallas.edu>
2014-12-09 20:02:45 -0600

    Set up initial view controllers

--- Infection/LimitedInfection/INFLimitedInfectionViewController.h
+++ Infection/LimitedInfection/INFLimitedInfectionViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  INFLimitedInfectionViewController.h
 //  Infection
 //
 //  Created by Justin Ehlert on 12/9/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface INFLimitedInfectionViewController : UIViewController
 
 @end
-
---
+++ Infection/TotalInfection/INFTotalInfectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  INFTotalInfectionViewController.h
+//  Infection
+//
+//  Created by Justin Ehlert on 12/9/14.
+//  Copyright (c) 2014 Justin Ehlert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface INFTotalInfectionViewController : UIViewController
+
+@end
